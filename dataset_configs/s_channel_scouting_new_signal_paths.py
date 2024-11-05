63302cea87b1ddfa808ec65e3feed2c3910b4177
mgaisd <marcel.gaisdoerfer@student.kit.edu>
2024-11-05 17:04:26 +0100

    changes to create file lists

--- dataset_configs/s_channel_scouting_new_signal_paths.py
+++ dataset_configs/s_channel_scouting_new_signal_paths.py
@@ -26,7 +26,7 @@ datasets_info = {
 }
 
 # SVJ scouting signal models
-signal_models1 = [
+signal_models = [
     "s-channel_mMed-700_mDark-20_rinv-0.3",
     "s-channel_mMed-700_mDark-20_rinv-0.5",
     "s-channel_mMed-700_mDark-20_rinv-0.7",
@@ -42,6 +42,22 @@ signal_models1 = [
     "s-channel_mMed-1000_mDark-20_rinv-0.3",
     "s-channel_mMed-1000_mDark-20_rinv-0.5",
     "s-channel_mMed-1000_mDark-20_rinv-0.7",
+
+    "s-channel_mMed-1100_mDark-20_rinv-0.3",
+    "s-channel_mMed-1100_mDark-20_rinv-0.5",
+    "s-channel_mMed-1100_mDark-20_rinv-0.7",
+
+    "s-channel_mMed-1200_mDark-20_rinv-0.3",
+    "s-channel_mMed-1200_mDark-20_rinv-0.5",
+    "s-channel_mMed-1200_mDark-20_rinv-0.7",
+
+    "s-channel_mMed-1300_mDark-20_rinv-0.3",
+    "s-channel_mMed-1300_mDark-20_rinv-0.5",
+    "s-channel_mMed-1300_mDark-20_rinv-0.7",
+
+    "s-channel_mMed-1400_mDark-20_rinv-0.3",
+    "s-channel_mMed-1400_mDark-20_rinv-0.5",
+    "s-channel_mMed-1400_mDark-20_rinv-0.7",
 
     "s-channel_mMed-1500_mDark-20_rinv-0.3",
     "s-channel_mMed-1500_mDark-20_rinv-0.5",
@@ -52,61 +68,61 @@ signal_models1 = [
     "s-channel_mMed-3000_mDark-20_rinv-0.7",
 ]
 
-signal_models2 = [
-    "s-channel_mMed-700_mDark-20_rinv-0.3",
-    "s-channel_mMed-700_mDark-20_rinv-0.5",
-    "s-channel_mMed-700_mDark-20_rinv-0.7",
+# signal_models2 = [
+#     "SVJ_hadronic_std2_s-channel_mMed-700_mDark-20_rinv-0.3",
+#     "SVJ_hadronic_std2_s-channel_mMed-700_mDark-20_rinv-0.5",
+#     "SVJ_hadronic_std2_s-channel_mMed-700_mDark-20_rinv-0.7",
 
-    "s-channel_mMed-800_mDark-20_rinv-0.3",
-    "s-channel_mMed-800_mDark-20_rinv-0.5",
-    "s-channel_mMed-800_mDark-20_rinv-0.7",
+#     "SVJ_hadronic_std2_s-channel_mMed-800_mDark-20_rinv-0.3",
+#     "SVJ_hadronic_std2_s-channel_mMed-800_mDark-20_rinv-0.5",
+#     "SVJ_hadronic_std2_s-channel_mMed-800_mDark-20_rinv-0.7",
 
-    "s-channel_mMed-900_mDark-20_rinv-0.3",
-    "s-channel_mMed-900_mDark-20_rinv-0.5",
-    "s-channel_mMed-900_mDark-20_rinv-0.7",
+#     "SVJ_hadronic_std2_s-channel_mMed-900_mDark-20_rinv-0.3",
+#     "SVJ_hadronic_std2_s-channel_mMed-900_mDark-20_rinv-0.5",
+#     "SVJ_hadronic_std2_s-channel_mMed-900_mDark-20_rinv-0.7",
 
-    "s-channel_mMed-1000_mDark-20_rinv-0.3",
-    "s-channel_mMed-1000_mDark-20_rinv-0.5",
-    "s-channel_mMed-1000_mDark-20_rinv-0.7",
+#     "SVJ_hadronic_std2_s-channel_mMed-1000_mDark-20_rinv-0.3",
+#     "SVJ_hadronic_std2_s-channel_mMed-1000_mDark-20_rinv-0.5",
+#     "SVJ_hadronic_std2_s-channel_mMed-1000_mDark-20_rinv-0.7",
 
-    "s-channel_mMed-1500_mDark-20_rinv-0.3",
-    "s-channel_mMed-1500_mDark-20_rinv-0.5",
-    "s-channel_mMed-1500_mDark-20_rinv-0.7",
-]
+#     "SVJ_hadronic_std2_s-channel_mMed-1500_mDark-20_rinv-0.3",
+#     "SVJ_hadronic_std2_s-channel_mMed-1500_mDark-20_rinv-0.5",
+#     "SVJ_hadronic_std2_s-channel_mMed-1500_mDark-20_rinv-0.7",
+# ]
 
-signal_models3 = [
-    "s-channel_mMed-700GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-700GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-700GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+# signal_models3 = [
+#     "s-channel_mMed-700GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-700GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-700GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
 
-    "s-channel_mMed-800GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-800GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-800GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-800GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-800GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-800GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
 
-    "s-channel_mMed-900GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-900GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-900GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-900GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-900GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-900GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
 
-    "s-channel_mMed-1100GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1100GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1100GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1100GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1100GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1100GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
 
-    "s-channel_mMed-1200GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1200GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1200GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1200GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1200GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1200GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
 
-    "s-channel_mMed-1300GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1300GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1300GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1300GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1300GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1300GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
 
-    "s-channel_mMed-1400GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1400GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1400GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1400GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1400GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1400GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
 
-    "s-channel_mMed-1500GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1500GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
-    "s-channel_mMed-1500GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
-]
+#     "s-channel_mMed-1500GeV_mDark-20GeV_rinv-0.3_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1500GeV_mDark-20GeV_rinv-0.5_alpha-peak_13TeV-pythia8_n-2000",
+#     "s-channel_mMed-1500GeV_mDark-20GeV_rinv-0.7_alpha-peak_13TeV-pythia8_n-2000",
+# ]
 
 qcd_bins = [
     "QCD_HT100to200",
@@ -132,43 +148,76 @@ wjets_bins = [
 ]
 
 for year in years:
-    datasets_info[year].update({
-        signal_model: [
-            {
-                "redirector": "root://storage01.lcg.cscs.ch:1096//",
-                "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std/{signal_model}/",
-                "regex": f"",
-                
-            },
-        ]
-        for signal_model in signal_models1
-    })
+    for signal_model in signal_models:
+        if "3000" in signal_model:
+            datasets_info[year].update({
+                signal_model: [
+                    {
+                        "redirector": "root://storage01.lcg.cscs.ch:1096//",
+                        "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std/{signal_model}/",
+                        "regex": f"",
+                    },
+                ]
+            })
+        elif "1000" in signal_model:
+            datasets_info[year].update({
+                signal_model: [
+                    {
+                        "redirector": "root://storage01.lcg.cscs.ch:1096//",
+                        "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std/{signal_model}/",
+                        "regex": f"",
+                    },
+                    {
+                        "redirector": "root://storage01.lcg.cscs.ch:1096//",
+                        "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std2/{signal_model}/",
+                        "regex": f"",
+                    
+                    },
+                ]
+            })
+        elif ("1100" in signal_model) or ("1200" in signal_model) or ("1300" in signal_model) or ("1400" in signal_model):
+            datasets_info[year].update({
+                signal_model: [
+                    {
+                        "redirector": "root://storage01.lcg.cscs.ch:1096//",
+                        "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std3/{signal_model}/",
+                        "regex": f"",
+                    },
+                ]
+            })
+        else:
+            datasets_info[year].update({
+                signal_model: [
+                    {
+                        "redirector": "root://storage01.lcg.cscs.ch:1096//",
+                        "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std/{signal_model}/",
+                        "regex": f"",
+                    },
+                    {
+                        "redirector": "root://storage01.lcg.cscs.ch:1096//",
+                        "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std2/{signal_model}/",
+                        "regex": f"",
+                    
+                    },
+                    {
+                        "redirector": "root://storage01.lcg.cscs.ch:1096//",
+                        "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std3/{signal_model}_alpha-peak_13TeV-pythia8_n-2000/",
+                        "regex": f"",
+                    },
+                ]
+            })         
 
-for year in years:
-    datasets_info[year].update({
-        signal_model: [
-            {
-                "redirector": "root://storage01.lcg.cscs.ch:1096//",
-                "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std2/{signal_model}/",
-                "regex": f"",
-                
-            },
-        ]
-        for signal_model in signal_models2
-    })
-
-for year in years:
-    datasets_info[year].update({
-        signal_model: [
-            {
-                "redirector": "root://storage01.lcg.cscs.ch:1096//",
-                "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std3/{signal_model}/",
-                "regex": f"",
-                
-            },
-        ]
-        for signal_model in signal_models3
-    })
+# for year in years:
+#     datasets_info[year].update({
+#         signal_model: [
+#             {
+#                 "redirector": "root://storage01.lcg.cscs.ch:1096//",
+#                 "path": f"/pnfs/lcg.cscs.ch/cms/trivcat/store/user/cazzanig/darkshowers/samples/scouting/PFNano/signals/SVJ_hadronic_std3/{signal_model}/",
+#                 "regex": f"",
+#             },
+#         ]
+#         for signal_model in signal_models3
+#     })
 
 
 for year in years:
