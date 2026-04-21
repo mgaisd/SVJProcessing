a4f8652213b33c319e5fe1b29f9ec39b97ee27b4
mgaisd <marcel.gaisdoerfer@student.kit.edu>
2026-04-21 16:31:58 +0200

    added first versions of lepton ids and ak4 jet id

--- analysis_configs/objects_definition_s_channel_scouting.py
+++ analysis_configs/objects_definition_s_channel_scouting.py
@@ -11,11 +11,24 @@ def is_analysis_ak8_jet(jets_ak8):
 
 
 def is_good_ak4_jet(jets):
+    return jets.id == 1
+
+
+def is_analysis_ak4_jet(jets):
     return (
         (jets.pt > 30)
         & (abs(jets.eta) < 2.4)
-        & (jets.ID == 1)
     )
+
+
+def is_good_electron(electrons):
+    # TODO: compute electron ID from scouting variables (analogous to add_jet_id_branch for ak8)
+    return electrons.id == 1
+
+
+def is_good_muon(muons):
+    # TODO: compute muon ID from scouting variables (analogous to add_jet_id_branch for ak8)
+    return muons.id == 1
 
 
 def is_analysis_electron(electrons):
@@ -46,50 +59,50 @@ def is_veto_muon(muons):
     )
 
 
-def is_tag_electron(electrons):
-    return (
-        is_analysis_electron(electrons)
-        & (electrons.mediumID == 1)
-        & (electrons.iso < 0.1)  # mini-isolation, tight WP
-    )
+# def is_tag_electron(electrons):
+#     return (
+#         is_analysis_electron(electrons)
+#         & (electrons.mediumID == 1)
+#         & (electrons.iso < 0.1)  # mini-isolation, tight WP
+#     )
 
 
-def is_tag_muon(muons):
-    return (
-        is_analysis_muon(muons)
-        & (muons.mediumID == 1)
-        & (muons.iso < 0.1)  # mini-isolation, tight WP
-    )
+# def is_tag_muon(muons):
+#     return (
+#         is_analysis_muon(muons)
+#         & (muons.mediumID == 1)
+#         & (muons.iso < 0.1)  # mini-isolation, tight WP
+#     )
 
 
-def __is_isolated_electron(electrons):
-    return electrons.pfRelIso < 0.15
+# def __is_isolated_electron(electrons):
+#     return electrons.pfRelIso < 0.15
 
 
-def __is_isolated_muon(muons):
-    return muons.pfRelIso < 0.15
+# def __is_isolated_muon(muons):
+#     return muons.pfRelIso < 0.15
 
 
-def is_cleaning_electron(electrons):
-    return (
-        is_tag_electron(electrons)
-        & __is_isolated_electron(electrons)
-    )
+# def is_cleaning_electron(electrons):
+#     return (
+#         is_tag_electron(electrons)
+#         & __is_isolated_electron(electrons)
+#     )
 
 
-def is_cleaning_muon(muons):
-    return (
-        is_tag_muon(muons)
-        & __is_isolated_muon(muons)
-    )
+# def is_cleaning_muon(muons):
+#     return (
+#         is_tag_muon(muons)
+#         & __is_isolated_muon(muons)
+#     )
 
 
-def is_good_photon(photons):
-    filter = (
-        (abs(photons.eta) < 2.5)  # Photon in ECAL
-        & ((abs(photons.eta) < 1.4442) | (abs(photons.eta) > 1.566))  # Exclude barrel-endcap transition 
-        & (photons.cutBasedID >= 2)  # Medium ID
-        & (photons.hasPixelSeed == False)  # No pixel seed -> conversion veto
-    )
-    return filter
+# def is_good_photon(photons):
+#     filter = (
+#         (abs(photons.eta) < 2.5)  # Photon in ECAL
+#         & ((abs(photons.eta) < 1.4442) | (abs(photons.eta) > 1.566))  # Exclude barrel-endcap transition 
+#         & (photons.cutBasedID >= 2)  # Medium ID
+#         & (photons.hasPixelSeed == False)  # No pixel seed -> conversion veto
+#     )
+#     return filter
 
--- analysis_configs/sequences_s_channel_scouting.py
+++ analysis_configs/sequences_s_channel_scouting.py
@@ -3,27 +3,19 @@ import awkward as ak
 
 from skimmer import skimmer_utils
 from utils.variables_computation import event_variables as event_vars
-from utils.variables_computation import jet_variables as jet_vars
 from utils.data.triggers import primary_dataset_triggers
 from utils.tree_maker.triggers import trigger_table
 from utils.awkward_array_utilities import as_type
-import analysis_configs.physics_objects_nano_aod as physicsObj
+# from utils.variables_computation import jet_variables as jet_vars
+# import analysis_configs.physics_objects_nano_aod as physicsObj
 from analysis_configs import objects_definition_s_channel_scouting as obj
 
 from utils.Logger import *
 
 
-def __unflatten_to_event_level(jet_variable,n_jets):
-    arrays = [ak.unflatten(jet_variable, n_jets, axis=0)]
-
-    if len(arrays) == 1:
-        return arrays[0]
-    else:
-        return arrays
-
 def apply_good_ak8_jet_filter(events):
-    events = add_branches_for_ak8_jet_id(events)
-    events = add_jet_id_branch(events)
+    # events = add_branches_for_ak8_jet_id(events) # reads variables from ntuplizer now, no need to recompute them
+    events = add_ak8_jet_id_branch(events)
     ak8_jets = ak.zip(
         {
             "pt": events.FatJet_pt,
@@ -42,59 +34,133 @@ def apply_good_ak8_jet_filter(events):
     return events
 
 
-def add_branches_for_ak8_jet_id(events):
-        
-        jet_collection_name = "FatJet"
-        constituent_collection_name = "PFCands"
-        jets = physicsObj.get_jets(events, jet_collection_name)
-        jet_pf_cands = physicsObj.get_jet_pf_cands(events, jet_collection_name, constituent_collection_name)
-        jet_pf_cands_per_jet, _ = jet_vars.make_constituents_per_jet(jet_pf_cands, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
-        flat_jets = ak.flatten(jets)
+def add_ak8_jet_id_branch(events):
+    CHM = events.FatJet_chargedMultiplicity
+    NumConst = events.FatJet_nConstituents
 
-        variables_fractions = ["chHEF", "neHEF", "electron_energy_fraction", "muon_energy_fraction", "photon_energy_fraction"]
+    passID = ((abs(events.FatJet_eta) <= 2.6)
+                   & (CHM > 0)
+                   & (events.FatJet_chargedHadronEnergyFraction > 0)
+                   & (events.FatJet_neutralHadronEnergyFraction < 0.9)
+                   & (NumConst > 1)
+                   & (events.FatJet_photonEnergyFraction < 0.9)
+                   & (events.FatJet_muonEnergyFraction < 0.8)
+                   & (events.FatJet_electronEnergyFraction < 0.8))
 
-        for variable_name in variables_fractions:
-            function = getattr(jet_vars, f"calculate_{variable_name}")
-            variable = function(jet_pf_cands_per_jet, flat_jets)
-            branch_name = f"FatJet_{variable_name.replace('_','')}"
-            events[branch_name] = __unflatten_to_event_level(variable, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
-
-        variables_multiplicities = ["electron_multiplicity", "muon_multiplicity", "chargedhadron_multiplicity"]
-
-        for variable_name in variables_multiplicities:
-            function = getattr(jet_vars, f"calculate_{variable_name}")
-            variable = function(jet_pf_cands_per_jet)
-            branch_name = f"{jet_collection_name}_{variable_name.replace('_','')}"
-            events[branch_name] = __unflatten_to_event_level(variable, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
-
-        #total PFCands multiplicity per jet
-        function = getattr(jet_vars, "calculate_multiplicity")
-        variable = function(jet_pf_cands_per_jet)
-        branch_name = f"{jet_collection_name}_multiplicity"
-        events[branch_name] = __unflatten_to_event_level(variable, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
-
-        return events
-
-def add_jet_id_branch(events):
-    #CZZ: here custom implementation of id function from offline analysis, to be substituted with variable in ntuplizer
-    # passID
-    CHM = events.FatJet_chargedhadronmultiplicity +events.FatJet_electronmultiplicity + events.FatJet_muonmultiplicity
-
-    passID = ((abs(events.FatJet_eta)<=2.6) 
-                   & (CHM>0) 
-                   & (events.FatJet_chHEF>0) 
-                   & (events.FatJet_neHEF<0.9) 
-                   & (events.FatJet_multiplicity>1)
-                   & (events.FatJet_photonenergyfraction<0.9)
-                   & (events.FatJet_muonenergyfraction<0.8) 
-                   & (events.FatJet_electronenergyfraction<0.8))  
-
-    #convert passID to 0 and 1
-    passID = as_type(passID,int)
+    passID = as_type(passID, int)
 
     events["FatJet_jetId"] = passID
 
     return events
+
+
+# def __unflatten_to_event_level(jet_variable, n_jets):
+#     arrays = [ak.unflatten(jet_variable, n_jets, axis=0)]
+#     if len(arrays) == 1:
+#         return arrays[0]
+#     else:
+#         return arrays
+#
+# def add_branches_for_ak8_jet_id(events):
+#     jet_collection_name = "FatJet"
+#     constituent_collection_name = "PFCands"
+#     jets = physicsObj.get_jets(events, jet_collection_name)
+#     jet_pf_cands = physicsObj.get_jet_pf_cands(events, jet_collection_name, constituent_collection_name)
+#     jet_pf_cands_per_jet, _ = jet_vars.make_constituents_per_jet(jet_pf_cands, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
+#     flat_jets = ak.flatten(jets)
+#
+#     variables_fractions = ["chHEF", "neHEF", "electron_energy_fraction", "muon_energy_fraction", "photon_energy_fraction"]
+#     for variable_name in variables_fractions:
+#         function = getattr(jet_vars, f"calculate_{variable_name}")
+#         variable = function(jet_pf_cands_per_jet, flat_jets)
+#         branch_name = f"FatJet_{variable_name.replace('_','')}"
+#         events[branch_name] = __unflatten_to_event_level(variable, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
+#
+#     variables_multiplicities = ["electron_multiplicity", "muon_multiplicity", "chargedhadron_multiplicity"]
+#     for variable_name in variables_multiplicities:
+#         function = getattr(jet_vars, f"calculate_{variable_name}")
+#         variable = function(jet_pf_cands_per_jet)
+#         branch_name = f"{jet_collection_name}_{variable_name.replace('_','')}"
+#         events[branch_name] = __unflatten_to_event_level(variable, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
+#
+#     function = getattr(jet_vars, "calculate_multiplicity")
+#     variable = function(jet_pf_cands_per_jet)
+#     branch_name = f"{jet_collection_name}_multiplicity"
+#     events[branch_name] = __unflatten_to_event_level(variable, n_jets=ak.count(events[jet_collection_name+"_pt"], axis=-1))
+#     return events
+#
+# def add_ak8_jet_id_branch_from_pf_cands(events):
+#     # Old approach: recompute ID variables from PF candidates
+#     events = add_branches_for_ak8_jet_id(events)
+#     CHM = events.FatJet_chargedhadronmultiplicity + events.FatJet_electronmultiplicity + events.FatJet_muonmultiplicity
+#     passID = ((abs(events.FatJet_eta) <= 2.6)
+#                    & (CHM > 0)
+#                    & (events.FatJet_chHEF > 0)
+#                    & (events.FatJet_neHEF < 0.9)
+#                    & (events.FatJet_multiplicity > 1)
+#                    & (events.FatJet_photonenergyfraction < 0.9)
+#                    & (events.FatJet_muonenergyfraction < 0.8)
+#                    & (events.FatJet_electronenergyfraction < 0.8))
+#     passID = as_type(passID, int)
+#     events["FatJet_jetId"] = passID
+#     return events
+
+
+def add_ak4_jet_id_branch(events):
+    CHM = events.Jet_chargedMultiplicity
+    NumConst = events.Jet_nConstituents
+
+    passID = ((abs(events.Jets_eta) <= 2.6)
+                   & (CHM > 0)
+                   & (events.Jet_chargedHadronEnergyFraction > 0)
+                   & (events.Jet_neutralHadronEnergyFraction < 0.9)
+                   & (NumConst > 1)
+                   & (events.Jet_photonEnergyFraction < 0.9)
+                   & (events.Jet_muonEnergyFraction < 0.8)
+                   & (events.Jet_electronEnergyFraction < 0.8))
+
+    passID = as_type(passID, int)
+
+    events["Jets_jetId"] = passID
+
+    return events
+
+def _compute_electron_id(events):
+    # Veto electron ID based on offline, had to remove relIso and pass conversion ratio requirements
+    # Barrel: |eta| < 1.479, Endcap: 1.479 < |eta| < 2.5
+    is_barrel = abs(events["Electron_eta"]) < 1.479
+
+    # Approximate energy: E_SC ~ pt * cosh(eta) (not really supercluster energy, but best proxy)
+    e_sc = events["Electron_pt"] * np.cosh(events["Electron_eta"])
+    # rho: median energy density, used for pile-up correction in H/E
+    rho = events["rho"]
+
+    sieie_cut = ak.where(is_barrel, 0.0126, 0.0457)
+    detain_cut = ak.where(is_barrel, 0.00463, 0.00814)
+    dphiin_cut = ak.where(is_barrel, 0.148, 0.19)
+    hoe_cut = ak.where(is_barrel, 0.05 + 1.16 / e_sc + 0.0324 * rho / e_sc, 0.05 + 2.54 / e_sc + 0.183 * rho / e_sc)
+    ooEMOop_cut = ak.where(is_barrel, 0.209, 0.132)
+    mhits_cut = ak.where(is_barrel, 2, 3)
+
+    passID = (
+        (events["Electron_sieie"] < sieie_cut)
+        & (abs(events["Electron_detain"]) < detain_cut)
+        & (abs(events["Electron_dphiin"]) < dphiin_cut)
+        & (events["Electron_hoe"] < hoe_cut)
+        & (abs(events["Electron_ooEMOop"]) < ooEMOop_cut)
+        & (events["Electron_mHits"] <= mhits_cut)
+    )
+    return as_type(passID, int)
+
+
+def _compute_muon_id(events):
+    # Loose muon ID: global muon or tracker muon
+    passID = (
+        events["Muon_isGlobalMuon"]
+        | events["Muon_isTrackerMuon"]
+    )
+    return as_type(passID, int)
+
 
 def add_good_ak8_jet_branch(events):
     ak8_jets = ak.zip(
@@ -119,6 +185,7 @@ def add_good_ak8_jet_branch(events):
     return events
 
 def add_good_ak4_jet_branch(events):
+    events = add_ak4_jet_id_branch(events)
     ak4_jets = ak.zip(
         {
             "pt": events.Jets_pt,
@@ -132,6 +199,7 @@ def add_good_ak4_jet_branch(events):
     
     is_good_analysis_ak4_jet = (
         obj.is_good_ak4_jet(ak4_jets)
+        & obj.is_analysis_ak4_jet(ak4_jets)
     )
 
     #add new branch to the events
@@ -157,9 +225,18 @@ def _build_scouting_lepton_collections(events):
         "Electron_pt",
         "Electron_eta",
         "Electron_combinedMiniIso",
+        "Electron_sieie",
+        "Electron_detain",
+        "Electron_dphiin",
+        "Electron_hoe",
+        "Electron_ooEMOop",
+        "Electron_mHits",
+        "rho",
         "Muon_pt",
         "Muon_eta",
         "Muon_combinedMiniIso",
+        "Muon_isGlobalMuon",
+        "Muon_isTrackerMuon",
     ]
 
     if any(field not in events.fields for field in required_fields):
@@ -170,6 +247,7 @@ def _build_scouting_lepton_collections(events):
             "pt": events["Electron_pt"],
             "eta": events["Electron_eta"],
             "iso": events["Electron_combinedMiniIso"],
+            "id": _compute_electron_id(events),
         }
     )
 
@@ -178,6 +256,7 @@ def _build_scouting_lepton_collections(events):
             "pt": events["Muon_pt"],
             "eta": events["Muon_eta"],
             "iso": events["Muon_combinedMiniIso"],
+            "id": _compute_muon_id(events),
         }
     )
 
